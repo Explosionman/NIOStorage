4d6cfb4e06b8dfb4bbdd4a13a638a161c6697191
Alexandr Rybinskov <rybinskov@gmail.com>
2020-07-26 12:14:11 +0300

    first

---
+++ NIOclient/src/main/java/NIOClient.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class NIOClient {
+    public static void main(String[] args) throws IOException, InterruptedException {
+}
+
